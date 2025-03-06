cc2d9c898876e3a28f33aa52886b57d562ca625e
Nate0309 <omoorenathan848@gmail.com>
2025-03-06 18:46:15 +0200

    Exercise 2 completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Greetings user, what percent did you receive for your test?");
+        string grade = Console.ReadLine();
+        int perc = int.Parse(grade);
+
+        string letter = "";
+
+        if (perc >= 90) 
+        {
+            letter = "A";
+        }
+        else if (perc >= 80)
+        {
+            letter = "B";
+        }
+        else if (perc >= 70)
+        {
+            letter = "C";
+        }
+        else if (perc >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        if (perc > 70)
+        {
+            Console.WriteLine($"Congratulations! You passed your test and received a(n) {letter}");
+        }
+        else
+        {
+            Console.WriteLine($"Sorry friend, you didn't pass this one. You only got a(n) {letter}. You'll get it next time;)");
+        }
+
     }
 }
