edfa5772caf949f20e09b3d5a326d15996f8f0cb
Nate0309 <omoorenathan848@gmail.com>
2025-03-07 15:17:21 +0200

    Completed exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+        do
+        {
+            Console.WriteLine("Enter number");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }    
+        }  while (number != 0);
+
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
+        double ave = numbers.Average();
+        Console.WriteLine($"The average is {ave}");
+
+        int largest = numbers.Max();
+        Console.WriteLine($"The largest number is {largest}");
     }
 }
